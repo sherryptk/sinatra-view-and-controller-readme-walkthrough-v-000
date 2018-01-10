5a5e496c10ec1f1fffe9a834fc5558bf49e9813e
Sherry El Garhy <sherryptk@gmail.com>
2018-01-10 21:59:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,9 +6,10 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-   puts params
-
-   erb :reversed
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+ 
+    erb :reversed
   end
 
   get '/friends' do
